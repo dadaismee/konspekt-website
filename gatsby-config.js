5496657ab1888931ca0ceb0edb683d060aa62ac1
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2023-01-27 14:54:47 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `konspekt-web`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag"]
+};
